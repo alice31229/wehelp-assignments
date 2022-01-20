5f36c352201d14b7a652dff867a36172e3522dc4
alice31229 <51815935+alice31229@users.noreply.github.com>
2022-01-20 09:52:06 +0800

    Add files via upload

--- week-4/app.py
+++ week-4/app.py
@@ -23,31 +23,48 @@ def login():
         session['LogIn'] = True
         return redirect(url_for('succeed'))
     else:
+        #return redirect('/error')
+
         if input_userName=='' or input_password=='':
             error_message = '請輸入帳號、密碼'
-         
+            #return redirect(url_for('fail',message=error_message))
         else:
             error_message = '帳號、或密碼輸入錯誤'
         #return redirect(f'/error/?message={error_message}')
         return redirect(url_for('fail',message=error_message))
 
+        #session['username'] = input_userName
+        #session['password'] = input_password
+        #return redirect('/error/')
 
-@app.route('/member')
+
+@app.route('/member/')
 def succeed():
     if 'LogIn' in session:
         return render_template('succeed.html')
     else:
         return redirect(url_for('index'))
 
-#@app.route("/error/?message=請輸入帳號、密碼")  #--> 找不到網址
-@app.route("/error/?message=<message>")  # -->  http://127.0.0.1:3000/error/%3Fmessage%3D請輸入帳號、密碼
-#@app.route("/error/<message>")  #--> http://127.0.0.1:3000/error/帳號、或密碼輸入錯誤
-def fail(message):
-    
-    #errMsg = request.args.get('message')  # --> None
-    
-    return render_template('fail.html',message=message)
 
+#@app.route("/error/<message>")
+@app.route('/error/')
+#def fail(message):
+def fail():
+    #return render_template('fail.html',message=message)
+    error_query_string = request.args['message']
+    return render_template('fail.html',message=error_query_string)
+
+    # try:
+    #     error_query_string = request.args['message']
+    #     return render_template('fail.html',message=error_query_string)
+    # except:
+    #     #userName = session['username']
+    #     #password = session['password']
+    #     # if userName=='' or password=='':
+    #     #     error_message = '請輸入帳號、密碼'
+    #     # else:
+    #     #     error_message = '帳號、或密碼輸入錯誤'
+    #    return render_template('fail.html')
 
 @app.route('/signout')
 def logOut():
